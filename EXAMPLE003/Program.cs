3058d744b2d19dbc96d31b424ca6e19ca7efb8ad
Roman <rom.badrutdinov2012@yandex.ru>
2023-03-11 17:40:27 +0300

    add random

--- EXAMPLE003/Program.cs
+++ EXAMPLE003/Program.cs
@@ -1,4 +1,6 @@
-﻿int numder1 = 32;
-int number2 = 5;
+﻿int numder1 = new Random().Next(1,10); //1 2 3 ...9
+Console. WriteLine(numder1);
+int number2 = new Random().Next(1,10);
+Console. WriteLine(number2);
 int rusult = number2 + numder1;
-Console.WriteLine(rusult);
+Console. WriteLine(rusult);
